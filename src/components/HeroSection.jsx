0c849b5dd50c3ebe0d0ede2fa49506302d7c36e1
Shazli Hassan <sh_dl@hotmail.com>
2025-09-12 12:16:23 +0400

    hero section minor update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -22,7 +22,7 @@ export const HeroSection = () => {
             proper software
           </p>
           <div className="pt-4 opacity-0 animate-fade-in-delay-4">
-            <a href="#project" className="cosmic-button">
+            <a href="#projects" className="cosmic-button">
               View my work
             </a>
           </div>
