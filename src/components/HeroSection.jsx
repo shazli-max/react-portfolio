543687e48655569fcdb32d3e6f3413496e22bd57
Shazli Hassan <sh_dl@hotmail.com>
2025-09-15 20:58:55 +0400

    title and hero section alteration

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,7 +11,7 @@ export const HeroSection = () => {
           <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
             <span className="opacity-0 animate-fade-in">Hi, I'm</span>
             <span className="text-primary opacity-0 animate-fade-in-delay-1 ml-2">
-              Shazli
+              Shazali
             </span>
             <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
               Hassan
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> ShoshoTech </span>{" "}
+            <span className="text-glow text-foreground"> Shazali Tech </span>{" "}
             Portfolio
           </span>
         </a>
