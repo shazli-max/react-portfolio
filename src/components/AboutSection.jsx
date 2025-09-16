170edb38e37703446fc8f08076c9937a59bd9eb3
Shazli Hassan <sh_dl@hotmail.com>
2025-09-16 19:42:29 +0400

    file fix

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="/public/projects/SHAZALI HASSAN.pdf"
+                href="/public/projects/SHAZALIHASSAN.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 download
               >
