dd67b005de078ed30916431f7e55da49e529e473
Shazli Hassan <sh_dl@hotmail.com>
2025-09-16 19:51:04 +0400

    file name added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               <a
                 href="/public/projects/SHAZALIHASSAN.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
-                download
+                download="Shazali Hassan"
               >
                 {" "}
                 Download CV{" "}
